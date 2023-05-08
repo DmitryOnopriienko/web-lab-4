12c7ff602ca8dc1b0f26c9f7ef93ce66f57f8bbd
Dmitry Onopriienko <dmitry.onopriienko@gmail.com>
2023-05-08 12:24:30 +0300

    Add greeting page

--- src/main/java/com/mvc/weblab4/controller/MainController.java
+++ src/main/java/com/mvc/weblab4/controller/MainController.java
@@ -1,15 +1,24 @@
 package com.mvc.weblab4.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-@RequestMapping("/web4")
 public class MainController {
 
   @GetMapping
   public String index() {
     return "index";
+  }
+
+  @GetMapping("/hi")
+  public String sayHi(Model model,
+                      @RequestParam(value = "first-name", required = false) String firstName,
+                      @RequestParam(value = "last-name", required = false) String lastName) {
+    model.addAttribute("firstName", firstName);
+    model.addAttribute("lastName", lastName);
+    return "hi";
   }
 }
